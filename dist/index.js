389347b61bd56c01d3b199b50692d3f5c860a82f
liznadro.correa <estanislao.gomez@correoaiep.cl>
2023-03-03 06:35:56 -0300

    37.48  /

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
+const default_routes_1 = __importDefault(require("./routes/default.routes"));
 const server = new server_1.default(); //intanciar objeto de clase server 
+server.app.use('/', default_routes_1.default); //rutas por defecto 
+server.Start(() => {
+    console.log(`Servidor corriendo en puerto:${server.port}`);
+});
--- index.ts
+++ index.ts
@@ -1,4 +1,12 @@
 import Server from "./classes/server";
+import defaultRoutes from "./routes/default.routes";
 
 
 const server = new Server();//intanciar objeto de clase server 
+
+
+server.app.use('/',defaultRoutes);//rutas por defecto 
+
+server.Start(()=>{
+    console.log(`Servidor corriendo en puerto:${server.port}`)
+})
